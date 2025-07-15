e2cff82ed8bb66bdb32210d093f864be56e62fd0
sunethra <sunethraboganatahm9@gmail.com>
2025-07-15 23:21:58 +0530

    extra_models

--- apis/cars.py
+++ apis/cars.py
@@ -10,29 +10,45 @@ models = [
       "brand": "Tata",
       "model": "Nexon",
       "type": "5-seater",
-      "description": "Tata Motors is known for safety and performance."
+      "description": "Tata Nexon is known for its safety, compact design, and fuel efficiency."
+    },
+    {
+      "brand": "Tata",
+      "model": "Safari",
+      "type": "7-seater",
+      "description": "Tata Safari is a spacious SUV built for family adventures."
     },
     {
       "brand": "Audi",
       "model": "A4",
       "type": "5-seater",
-      "description": "Audi is a German brand known for luxury and innovation."
+      "description": "Audi A4 is a luxury sedan combining elegance and advanced tech."
+    },
+    {
+      "brand": "Audi",
+      "model": "Q5",
+      "type": "7-seater",
+      "description": "Audi Q5 is a premium SUV offering performance and comfort."
     },
     {
       "brand": "Benz",
       "model": "C-Class",
       "type": "5-seater",
-      "description": "Mercedes-Benz offers top-tier performance and comfort."
-    }]
+      "description": "Mercedes-Benz C-Class delivers style, comfort, and performance."
+    },
+    {
+      "brand": "Benz",
+      "model": "GLC",
+      "type": "7-seater",
+      "description": "Mercedes-Benz GLC is a luxury SUV built for dynamic driving experiences."
+    }
+  ]
 @app.get("/cars")
 def car_details(name: str):
-    res = {}
-    if (name == "Benz"):
-        res = models[2]
-    elif (name == "Tata"):
-        res = models[0]
-    else:
-        res = models[1]
+    res = []
+    for model in models:
+        if(model["brand"] == name):
+            res.append(model)
     return res
 
 
